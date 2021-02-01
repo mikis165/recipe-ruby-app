76bf9b5991a7765a7fea4cf433698e1561de9055
michele saladino <michelelewagon@gmail.com>
2021-02-01 06:46:22 +0100

    First part completed

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root "pages#home"
+  get 'pages/home', to: 'pages#home'
+
+  resources :recipes
+  
+  get '/signup', to: 'chefs#new'
+  resources :chefs, except: [:new]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
+
+
+end
